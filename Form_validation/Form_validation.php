7ff4fc1c4aed33c4181c286aa0b7eefa4e78b932
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-02-24 23:03:55 +0530

    validation function added

--- Form_validation/Form_validation.php
+++ Form_validation/Form_validation.php
@@ -8,44 +8,66 @@
     <title>Personal Detailes</title>
     <style>
         input[type="text"],
-        [type="tel"] ,
-        [type="email"]{
+        [type="tel"],
+        [type="email"],
+        [type="password"] {
             background: white;
             border-color: greenyellow;
             opacity: 0.8;
             height: 2rem;
             width: 15rem;
-            border-radius: 1px;
+            padding-left: 2rem;
+            border-radius: 20px;
         }
+
         input[type="submit"],
-        [type="Reset"]{
+        [type="Reset"] {
             background: green;
             color: white;
             border: none;
-            border-radius: 2rem;
+            border-radius: 1rem;
             padding: 1rem;
         }
-         body {
-            background: black;
-            color: white;
+
+        body {
+            background: white;
+            color: gray;
+        }
+
+        .section {
+            border: 2px solid black;
+            width: 25rem;
+            padding-bottom: 2rem;
+            margin-top: 4rem;
+
         }
     </style>
+
 </head>
+
 <body>
     <center>
-        <h1>Personal Details</h1>
-        <form action="" method="post">
-            <input type="text" placeholder="Enter your first name" name="Firstname" autofocus>
-            <input type="text" placeholder="Enter your last name" name="Lastname"><br><br>
-            <input type="email" placeholder="email@gmail.com" name="email" >
-            <input type="tel" name="phone" placeholder="Enter your phone number"><br><br>
-            <label>Male</label>
-            <input type="radio" name="gender" value="Male">
-            <label>Female</label>
-            <input type="radio" name="gender" value="Female"><br><br>
-            <input type="reset" value="Reset">
-            <input type="submit" value="Submit">
-        </form>
+        <section class="section">
+
+            <h1>Personal Details</h1>
+            <form action="" method="post">
+                <input type="text" placeholder="Enter your name...." id="Firstname" max="15" autofocus required><br><br>
+                <input type="email" placeholder="email@gmail.com" id="email" required><br><br>
+                <input type="tel" id="phone" placeholder="Enter your phone number" max="10" required><br><br>
+                <input type="password" id="pass_one" placeholder="Enter your password...." max="10" required><br><br>
+                <input type="password" id="pass_two" placeholder="Confirm your password..." max="10" required><br><br>
+                <input type="reset" value="Reset">
+                <input type="submit" value="Submit" onclick=validation()>
+            </form>
+        </section>
     </center>
+    <script>
+        function validation() {
+            var name=document.getElementById("Firstname").value;
+
+            alert(name + "hello");
+        }
+    </script>
 </body>
+
 </html>
