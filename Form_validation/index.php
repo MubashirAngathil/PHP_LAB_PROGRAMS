a87f03ceafa1d7ed3d2b38be79a63ee7ea8a53a7
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-02-25 00:29:29 +0530

    Form validation bug fixed

--- Form_validation/index.php
+++ Form_validation/index.php
@@ -28,7 +28,8 @@ Program : java script to form validation
         }
 
         input[type="submit"],
-        [type="Reset"] {
+        [type="Reset"],
+        [type="Button"] {
             background: green;
             color: white;
             border: none;
@@ -58,24 +59,57 @@ Program : java script to form validation
 
             <h1>Personal Details</h1>
             <form action="" method="post">
+                <label id="nameLabel"></label>
                 <input type="text" placeholder="Enter your name...." id="Firstname" max="15" autofocus required><br><br>
+                <label id="emailLabel"></label>
                 <input type="email" placeholder="email@gmail.com" id="email" required><br><br>
                 <input type="tel" id="phone" placeholder="Enter your phone number" max="10" required><br><br>
                 <input type="password" id="pass_one" placeholder="Enter your password...." max="10" required><br><br>
                 <input type="password" id="pass_two" placeholder="Confirm your password..." max="10" required><br><br>
                 <input type="reset" value="Reset">
-                <input type="submit" value="Submit" onclick=validation()>
-           
+                <input type="submit" value="Submit" id="submit" onclick=validation()>
             </form>
         </section>
     </center>
     <script>
         function validation() {
-            var name=document.getElementById("Firstname").value;
-           
-                alert("Max length 20!!");
-         
-            
+            // name validation
+            var label = document.getElementById("nameLabel");
+            var labele = document.getElementById("emailLabel");
+            var name = document.getElementById("Firstname");
+            var submit = document.getElementById("submit");
+            var mail = document.getElementById("email");
+            var pass = document.getElementById("pass_one");
+            var pass2 = document.getElementById("pass_two");
+            var len = name.value.length;
+
+            if (len > 5) {
+                alert("Max length 15!!");
+                submit.type = "button";
+                name.style.background = "red";
+                label.style.color = "red";
+                label.innerText = "*Error"
+                submit.style.backgroundColor = "red";
+            } else {
+                submit.type = "submit";
+                name.style.background = "white";
+                label.innerText = ""
+                submit.style.backgroundColor = "green";
+            }
+            // email validation
+            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+                alert("You have entered an invalid email address!");
+                labele.style.color = "red";
+                labele.style.innerText = "*Error";
+            }
+            if (pass.value === pass2.value && pass.value.length === pass2.value.length) {
+                alert(pass.value, pass2.value);
+            } else {
+                pass2.style.background = "red";
+                submit.type = "button";
+                name.style.background = "red";
+            }
+
         }
     </script>
 </body>
