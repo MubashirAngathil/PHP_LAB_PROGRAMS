305d5972fc355737653ec7c6ec9f99f82067a798
midlajc <mail.midlajc@gmail.com>
2022-02-28 23:02:42 +0530

    bio data align ++

--- Bio-Data/index.php
+++ Bio-Data/index.php
@@ -53,6 +53,14 @@ Program : Bio-data generation php
             padding: 1rem;
             width: 9.7rem
         }
+
+        .table {
+            width: 100%;
+        }
+
+        th {
+            width: 20%;
+        }
     </style>
 </head>
 
@@ -61,45 +69,117 @@ Program : Bio-data generation php
         <h1 align="center">Bio Data Form</h1>
 
         <form action="biodata.php" method="POST">
-            <label for="fullname">Full Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :</label>
-            <input type="text" name="fullname"><br><br>
-            <label for="fathername">Father's Name&nbsp;&nbsp;&nbsp;:</label>
-            <input type="text" name="fathername"><br><br>
-            <label for="mothername">Mother's Name&nbsp;&nbsp;:</label>
-            <input type="text" name="mothername"><br><br>
-            <label for="bgroup">Blood Group&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</label>
-            <select name="bgroup">
-                <option value="null" selected></option>
-                <?php
-                $bgroup = array('A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-');
-                foreach ($bgroup as $key) {
-                    $keylower = strtolower($key);
-                    echo "<option value=" . $keylower . ">" . $key . "</option>";
-                }
-                ?>
-            </select><br><br>
-            <label for="phnum">Phone Number&nbsp;&nbsp;&nbsp;:</label>
-            <input type="tel" name="phnum" pattern="[0-9]{4}[0-9]{3}[0-9]{3}"><br><br><br>
-            <label for="email">Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                &nbsp;&nbsp;&nbsp;&nbsp;:</label>
-            <input type="email" name="email"><br><br>
-            <label for="dob">D.O.B&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</label>
-            <input type="date" name="dob"><br><br>
-            <label for="gender">Gender&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</label>
-            <input type="radio" name="gender" value="Male" checked>Male
-            <input type="radio" name="gender" value="Female">Female
-            <input type="radio" name="gender" value="Other">Other<br><br>
-            <label for="address" id="txtarea">Address&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</label>
-            <textarea name="address" id="address" cols="30" rows="5" placeholder=" Type your address here....."></textarea><br><br>
-            <label for="pincode">Pincode&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</label>
-            <input type="number" name="pincode" placeholder="6 - - - - -"><br><br>
-            <label for="gender">Language Known&nbsp;&nbsp;:</label>
-            <input type="checkbox" name="lang[]" value="english">English
-            <input type="checkbox" name="lang[]" value="tamil">Tamil
-            <input type="checkbox" name="lang[]" value="hindi">Hindi
-            <input type="checkbox" name="lang[]" value="malayalam">Malayalam
-            <input type="checkbox" name="lang[]" value="kannada">Kannada
-            <input type="checkbox" name="lang[]" value="telugu">Telugu
+            <table class="table">
+                <tr>
+                    <th>
+                        <label for="fullname">Full Name:</label>
+                    </th>
+                    <td>
+                        <input type="text" name="fullname"><br><br>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="fathername">Father's Name:</label>
+                    </th>
+                    <td>
+                        <input type="text" name="fathername"><br><br>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="mothername">Mother's Name:</label>
+                    </th>
+                    <td>
+                        <input type="text" name="mothername"><br><br>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="bgroup">Blood Group:</label>
+                    </th>
+                    <td>
+
+                        <select name="bgroup">
+                            <option value="null" selected></option>
+                            <?php
+                            $bgroup = array('A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-');
+                            foreach ($bgroup as $key) {
+                                $keylower = strtolower($key);
+                                echo "<option value=" . $keylower . ">" . $key . "</option>";
+                            }
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="phnum">Phone Number:</label>
+                    </th>
+                    <td>
+                        <input type="tel" name="phnum" pattern="[0-9]{4}[0-9]{3}[0-9]{3}"><br><br><br>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="email">Email:</label>
+                    </th>
+                    <td>
+                        <input type="email" name="email"><br><br>
+                    </td>
+                </tr>
+                <tr>
+
+                    <th>
+                        <label for="dob">D.O.B:</label>
+                    </th>
+                    <td>
+                        <input type="date" name="dob"><br><br>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="gender">Gender:</label>
+                    </th>
+                    <td>
+                        <input type="radio" name="gender" value="Male" checked>Male
+                        <input type="radio" name="gender" value="Female">Female
+                        <input type="radio" name="gender" value="Other">Other<br><br>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="address" id="txtarea">Address:</label>
+                    </th>
+                    <td>
+                        <textarea name="address" id="address" cols="30" rows="5" placeholder=" Type your address here....."></textarea><br><br>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="pincode">Pincode:</label>
+
+                    </th>
+                    <td>
+                        <input type="number" name="pincode" placeholder="6 - - - - -"><br><br>
+
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <label for="">Language Known:</label>
+
+                    </th>
+                    <td>
+                        <input type="checkbox" name="lang[]" value="english">English
+                        <input type="checkbox" name="lang[]" value="tamil">Tamil
+                        <input type="checkbox" name="lang[]" value="hindi">Hindi
+                        <input type="checkbox" name="lang[]" value="malayalam">Malayalam
+                        <input type="checkbox" name="lang[]" value="kannada">Kannada
+                        <input type="checkbox" name="lang[]" value="telugu">Telugu
+                    </td>
+                </tr>
+            </table>
             <input type="submit" value="Submit">
             <input type="reset" value="Reset">
         </form>
